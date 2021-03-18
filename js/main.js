8f99f9a52147de863e0cd7a81128769dfc667d9c
Raide Junior <69761646+Raidejunior@users.noreply.github.com>
2021-03-18 02:02:45 -0300

    :up: Término das 2 páginas de simulação de empréstimo

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var valoremprestimo = document.getElementById('valoremprestimo');
 var tn_name = document.getElementById('tn_name');
 var valorimovel = document.getElementById('valorimovel');
 var valorimovel2 = document.getElementById('valorimovel2');
+var valoratualemprestimo;
+var valoratualimovel2;
+var month;
 
 var tunaWizard = {
     stepCount: 0,
@@ -56,6 +59,7 @@ var tunaWizard = {
             return false;
         }
 
+
         //Validations
         if (nextStep === 2) {
             if ($("#tn_name").val().trim() === "") {
@@ -68,7 +72,23 @@ var tunaWizard = {
                 return;
             }
 
+            if (($("#tn_name").val().trim() !== "") && ($("#valorimovel").val().trim() !== "")) {
+                if ($("#simulador").hasClass("d-none")) {
+                    $("#simulador").removeClass("d-none");
+                    $("#left-layout").addClass("d-none");
+                }
+            }
+
+
+        } else if (!($("#simulador").hasClass("d-none") && !($("#left-layout").hasClass("d-none")))) {
+            $("#simulador").addClass("d-none");
+            $("#left-layout").removeClass("d-none");
         }
+
+        calcualtePrice(tn_name.value, valorimovel.value);
+
+
+
         //Value validation
         if (nextStep === 3) {
 
@@ -461,9 +481,9 @@ $(document).ready(function() {
      */
     tunaWizard.start();
 
-});
+    /* -------------------------- Adicionados para o simulador ------------------------------------- */
 
-$(document).ready(function() {
+
 
 
     $("#formSimulacaoWeb").validate({
@@ -543,4 +563,432 @@ $(document).ready(function() {
         }
     }
 
+
+
+
+
+    $("#total").val("10000");
+
+    $('.month').on('click', function(event) {
+
+        var id = $(this).attr('id');
+
+        $('.month').removeClass('selected-month');
+        $(this).addClass('selected-month');
+        $(".month").removeClass("active-month");
+        $(this).addClass("active-month");
+
+        calcualtePrice(valoremprestimo.value, valorimovel2.value);
+    });
+
+    /* 
+        update();
+        calcualtePrice(); */
+
 });
+
+
+
+
+/* ----------------------------------------- Scripts adicionados ----------------------------------------- */
+
+jQuery(document).ready(function($) {
+    var timelines = $('.cd-horizontal-timeline'),
+        eventsMinDistance = 60;
+
+    (timelines.length > 0) && initTimeline(timelines);
+
+    function initTimeline(timelines) {
+        timelines.each(function() {
+            var timeline = $(this),
+                timelineComponents = {};
+            //cache timeline components 
+            timelineComponents['timelineWrapper'] = timeline.find('.events-wrapper');
+            timelineComponents['eventsWrapper'] = timelineComponents['timelineWrapper'].children('.events');
+            timelineComponents['fillingLine'] = timelineComponents['eventsWrapper'].children('.filling-line');
+            timelineComponents['timelineEvents'] = timelineComponents['eventsWrapper'].find('a');
+            timelineComponents['timelineDates'] = parseDate(timelineComponents['timelineEvents']);
+            timelineComponents['eventsMinLapse'] = minLapse(timelineComponents['timelineDates']);
+            timelineComponents['timelineNavigation'] = timeline.find('.cd-timeline-navigation');
+            timelineComponents['eventsContent'] = timeline.children('.events-content');
+
+            //assign a left postion to the single events along the timeline
+            setDatePosition(timelineComponents, eventsMinDistance);
+            //assign a width to the timeline
+            var timelineTotWidth = setTimelineWidth(timelineComponents, eventsMinDistance);
+            //the timeline has been initialize - show it
+            timeline.addClass('loaded');
+
+            //detect click on the next arrow
+            timelineComponents['timelineNavigation'].on('click', '.next', function(event) {
+                event.preventDefault();
+                updateSlide(timelineComponents, timelineTotWidth, 'next');
+            });
+            //detect click on the prev arrow
+            timelineComponents['timelineNavigation'].on('click', '.prev', function(event) {
+                event.preventDefault();
+                updateSlide(timelineComponents, timelineTotWidth, 'prev');
+            });
+            //detect click on the a single event - show new event content
+            timelineComponents['eventsWrapper'].on('click', 'a', function(event) {
+                event.preventDefault();
+                timelineComponents['timelineEvents'].removeClass('selected');
+                $(this).addClass('selected');
+                updateOlderEvents($(this));
+                updateFilling($(this), timelineComponents['fillingLine'], timelineTotWidth);
+                updateVisibleContent($(this), timelineComponents['eventsContent']);
+            });
+
+            //on swipe, show next/prev event content
+            timelineComponents['eventsContent'].on('swipeleft', function() {
+                var mq = checkMQ();
+                (mq == 'mobile') && showNewContent(timelineComponents, timelineTotWidth, 'next');
+            });
+            timelineComponents['eventsContent'].on('swiperight', function() {
+                var mq = checkMQ();
+                (mq == 'mobile') && showNewContent(timelineComponents, timelineTotWidth, 'prev');
+            });
+
+            //keyboard navigation
+            $(document).keyup(function(event) {
+                if (event.which == '37' && elementInViewport(timeline.get(0))) {
+                    showNewContent(timelineComponents, timelineTotWidth, 'prev');
+                } else if (event.which == '39' && elementInViewport(timeline.get(0))) {
+                    showNewContent(timelineComponents, timelineTotWidth, 'next');
+                }
+            });
+        });
+    }
+
+    function updateSlide(timelineComponents, timelineTotWidth, string) {
+        //retrieve translateX value of timelineComponents['eventsWrapper']
+        var translateValue = getTranslateValue(timelineComponents['eventsWrapper']),
+            wrapperWidth = Number(timelineComponents['timelineWrapper'].css('width').replace('px', ''));
+        //translate the timeline to the left('next')/right('prev') 
+        (string == 'next') ?
+        translateTimeline(timelineComponents, translateValue - wrapperWidth + eventsMinDistance, wrapperWidth - timelineTotWidth): translateTimeline(timelineComponents, translateValue + wrapperWidth - eventsMinDistance);
+    }
+
+    function showNewContent(timelineComponents, timelineTotWidth, string) {
+        //go from one event to the next/previous one
+        var visibleContent = timelineComponents['eventsContent'].find('.selected'),
+            newContent = (string == 'next') ? visibleContent.next() : visibleContent.prev();
+
+        if (newContent.length > 0) { //if there's a next/prev event - show it
+            var selectedDate = timelineComponents['eventsWrapper'].find('.selected'),
+                newEvent = (string == 'next') ? selectedDate.parent('li').next('li').children('a') : selectedDate.parent('li').prev('li').children('a');
+
+            updateFilling(newEvent, timelineComponents['fillingLine'], timelineTotWidth);
+            updateVisibleContent(newEvent, timelineComponents['eventsContent']);
+            newEvent.addClass('selected');
+            selectedDate.removeClass('selected');
+            updateOlderEvents(newEvent);
+            updateTimelinePosition(string, newEvent, timelineComponents);
+        }
+    }
+
+    function updateTimelinePosition(string, event, timelineComponents) {
+        //translate timeline to the left/right according to the position of the selected event
+        var eventStyle = window.getComputedStyle(event.get(0), null),
+            eventLeft = Number(eventStyle.getPropertyValue("left").replace('px', '')),
+            timelineWidth = Number(timelineComponents['timelineWrapper'].css('width').replace('px', '')),
+            timelineTotWidth = Number(timelineComponents['eventsWrapper'].css('width').replace('px', ''));
+        var timelineTranslate = getTranslateValue(timelineComponents['eventsWrapper']);
+
+        if ((string == 'next' && eventLeft > timelineWidth - timelineTranslate) || (string == 'prev' && eventLeft < -timelineTranslate)) {
+            translateTimeline(timelineComponents, -eventLeft + timelineWidth / 2, timelineWidth - timelineTotWidth);
+        }
+    }
+
+    function translateTimeline(timelineComponents, value, totWidth) {
+        var eventsWrapper = timelineComponents['eventsWrapper'].get(0);
+        value = (value > 0) ? 0 : value; //only negative translate value
+        value = (!(typeof totWidth === 'undefined') && value < totWidth) ? totWidth : value; //do not translate more than timeline width
+        setTransformValue(eventsWrapper, 'translateX', value + 'px');
+        //update navigation arrows visibility
+        (value == 0) ? timelineComponents['timelineNavigation'].find('.prev').addClass('inactive'): timelineComponents['timelineNavigation'].find('.prev').removeClass('inactive');
+        (value == totWidth) ? timelineComponents['timelineNavigation'].find('.next').addClass('inactive'): timelineComponents['timelineNavigation'].find('.next').removeClass('inactive');
+    }
+
+    function updateFilling(selectedEvent, filling, totWidth) {
+        //change .filling-line length according to the selected event
+        var eventStyle = window.getComputedStyle(selectedEvent.get(0), null),
+            eventLeft = eventStyle.getPropertyValue("left"),
+            eventWidth = eventStyle.getPropertyValue("width");
+        eventLeft = Number(eventLeft.replace('px', '')) + Number(eventWidth.replace('px', '')) / 2;
+        var scaleValue = eventLeft / totWidth;
+        setTransformValue(filling.get(0), 'scaleX', scaleValue);
+    }
+
+    function setDatePosition(timelineComponents, min) {
+        for (i = 0; i < timelineComponents['timelineDates'].length; i++) {
+            var distance = daydiff(timelineComponents['timelineDates'][0], timelineComponents['timelineDates'][i]),
+                distanceNorm = Math.round(distance / timelineComponents['eventsMinLapse']) + 2;
+            timelineComponents['timelineEvents'].eq(i).css('left', distanceNorm * min + 'px');
+        }
+    }
+
+    function setTimelineWidth(timelineComponents, width) {
+        var timeSpan = daydiff(timelineComponents['timelineDates'][0], timelineComponents['timelineDates'][timelineComponents['timelineDates'].length - 1]),
+            timeSpanNorm = timeSpan / timelineComponents['eventsMinLapse'],
+            timeSpanNorm = Math.round(timeSpanNorm) + 4,
+            totalWidth = timeSpanNorm * width;
+        timelineComponents['eventsWrapper'].css('width', totalWidth + 'px');
+        updateFilling(timelineComponents['eventsWrapper'].find('a.selected'), timelineComponents['fillingLine'], totalWidth);
+        updateTimelinePosition('next', timelineComponents['eventsWrapper'].find('a.selected'), timelineComponents);
+
+        return totalWidth;
+    }
+
+    function updateVisibleContent(event, eventsContent) {
+        var eventDate = event.data('date'),
+            visibleContent = eventsContent.find('.selected'),
+            selectedContent = eventsContent.find('[data-date="' + eventDate + '"]'),
+            selectedContentHeight = selectedContent.height();
+
+        if (selectedContent.index() > visibleContent.index()) {
+            var classEnetering = 'selected enter-right',
+                classLeaving = 'leave-left';
+        } else {
+            var classEnetering = 'selected enter-left',
+                classLeaving = 'leave-right';
+        }
+
+        selectedContent.attr('class', classEnetering);
+        visibleContent.attr('class', classLeaving).one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function() {
+            visibleContent.removeClass('leave-right leave-left');
+            selectedContent.removeClass('enter-left enter-right');
+        });
+    }
+
+    function updateOlderEvents(event) {
+        event.parent('li').prevAll('li').children('a').addClass('older-event').end().end().nextAll('li').children('a').removeClass('older-event');
+    }
+
+    function getTranslateValue(timeline) {
+        var timelineStyle = window.getComputedStyle(timeline.get(0), null),
+            timelineTranslate = timelineStyle.getPropertyValue("-webkit-transform") ||
+            timelineStyle.getPropertyValue("-moz-transform") ||
+            timelineStyle.getPropertyValue("-ms-transform") ||
+            timelineStyle.getPropertyValue("-o-transform") ||
+            timelineStyle.getPropertyValue("transform");
+
+        if (timelineTranslate.indexOf('(') >= 0) {
+            var timelineTranslate = timelineTranslate.split('(')[1];
+            timelineTranslate = timelineTranslate.split(')')[0];
+            timelineTranslate = timelineTranslate.split(',');
+            var translateValue = timelineTranslate[4];
+        } else {
+            var translateValue = 0;
+        }
+
+        return Number(translateValue);
+    }
+
+    function setTransformValue(element, property, value) {
+        element.style["-webkit-transform"] = property + "(" + value + ")";
+        element.style["-moz-transform"] = property + "(" + value + ")";
+        element.style["-ms-transform"] = property + "(" + value + ")";
+        element.style["-o-transform"] = property + "(" + value + ")";
+        element.style["transform"] = property + "(" + value + ")";
+    }
+
+    //based on http://stackoverflow.com/questions/542938/how-do-i-get-the-number-of-days-between-two-dates-in-javascript
+    function parseDate(events) {
+        var dateArrays = [];
+        events.each(function() {
+            var singleDate = $(this),
+                dateComp = singleDate.data('date').split('T');
+            if (dateComp.length > 1) { //both DD/MM/YEAR and time are provided
+                var dayComp = dateComp[0].split('/'),
+                    timeComp = dateComp[1].split(':');
+            } else if (dateComp[0].indexOf(':') >= 0) { //only time is provide
+                var dayComp = ["2000", "0", "0"],
+                    timeComp = dateComp[0].split(':');
+            } else { //only DD/MM/YEAR
+                var dayComp = dateComp[0].split('/'),
+                    timeComp = ["0", "0"];
+            }
+            var newDate = new Date(dayComp[2], dayComp[1] - 1, dayComp[0], timeComp[0], timeComp[1]);
+            dateArrays.push(newDate);
+        });
+        return dateArrays;
+    }
+
+    function daydiff(first, second) {
+        return Math.round((second - first));
+    }
+
+    function minLapse(dates) {
+        //determine the minimum distance among events
+        var dateDistances = [];
+        for (i = 1; i < dates.length; i++) {
+            var distance = daydiff(dates[i - 1], dates[i]);
+            dateDistances.push(distance);
+        }
+        return Math.min.apply(null, dateDistances);
+    }
+
+    /*
+    	How to tell if a DOM element is visible in the current viewport?
+    	http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+    */
+    function elementInViewport(el) {
+        var top = el.offsetTop;
+        var left = el.offsetLeft;
+        var width = el.offsetWidth;
+        var height = el.offsetHeight;
+
+        while (el.offsetParent) {
+            el = el.offsetParent;
+            top += el.offsetTop;
+            left += el.offsetLeft;
+        }
+
+        return (
+            top < (window.pageYOffset + window.innerHeight) &&
+            left < (window.pageXOffset + window.innerWidth) &&
+            (top + height) > window.pageYOffset &&
+            (left + width) > window.pageXOffset
+        );
+    }
+
+    function checkMQ() {
+        //check if mobile or desktop device
+        return window.getComputedStyle(document.querySelector('.cd-horizontal-timeline'), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "");
+    }
+});
+
+
+/* ---------------------------------------------- Cálculo empréstimo ---------------------------------------------- */
+
+
+var p = {
+
+    0: "100K",
+    1: "150K",
+    2: "200K",
+    3: "250K",
+    4: "300K",
+    5: "350K",
+    6: "400K",
+    7: "450K",
+    8: "500K",
+    9: "550K",
+    10: "600K",
+    11: "650K",
+    12: "700K",
+    13: "750K",
+    14: "800K",
+    15: "850K",
+    16: "900K",
+    17: "950K",
+    18: "1,000K",
+    19: "1,100K",
+    20: "1,200K",
+    21: "1,300K",
+    22: "1,400K",
+    23: "1,500K",
+    24: "1,600K",
+    25: "1,700K",
+    26: "1,800K",
+    27: "19,00K",
+    28: "2,000K",
+};
+
+/* var t = {
+
+    0: "100000",
+    1: "150000",
+    2: "200000",
+    3: "250000",
+    4: "300000",
+    5: "350000",
+    6: "400000",
+    7: "450000",
+    8: "500000",
+    9: "550000",
+    10: "600000",
+    11: "650000",
+    12: "700000",
+    13: "750000",
+    14: "800000",
+    15: "850000",
+    16: "900000",
+    17: "950000",
+    18: "1000000",
+    19: "1100000",
+    20: "1200000",
+    21: "1300000",
+    22: "1400000",
+    23: "1500000",
+    24: "1600000",
+    25: "1700000",
+    26: "1800000",
+    27: "1900000",
+    28: "2000000",
+
+} */
+
+var obj = {
+    '144month': '1.10',
+    '132month': '1.20',
+    '120month': '1.30',
+
+    '108month': '1.40',
+    '96month': '1.50',
+    '84month': '1.60',
+    '72month': '1.70',
+
+    '60month': '1.80',
+    '48month': '1.90',
+    '36month': '2.0'
+};
+
+
+
+
+
+
+/* $("#slider_amirol").slider({
+    range: "min",
+    animate: true,
+
+    min: 0,
+    max: 28,
+    step: 1,
+    slide: function(event, ui) {
+        update(1, ui.value); //changed
+        calcualtePrice(ui.value);
+    }
+}); */
+
+
+
+function calcualtePrice(valemp, valimo) {
+
+    if (undefined === valemp || undefined === valimo)
+        valemp = 0, valimo = 0;
+
+    valemp = Number(valemp.replace(/[^0-9,]*/g, '').replace(',', '.'));
+    valimo = Number(valimo.replace(/[^0-9,]*/g, '').replace(',', '.'));
+
+
+    month = $(".selected-month").attr('id');
+    var term = obj[month];
+    16, 567.64
+    var totalPrice = valemp * term / (valimo * 0.07);
+
+    $("#total").val(totalPrice.toFixed(2));
+    $("#total12").val(Math.round((totalPrice) / 12).toFixed(2));
+    $("#total52").val(Math.round((totalPrice) / 52).toFixed(2));
+}
+
+function alteravalor() {
+
+    valoratualemprestimo = $('#valoremprestimo').val();
+    valoratualimovel2 = $('#valorimovel2').val();
+
+    calcualtePrice(valoratualemprestimo, valoratualimovel2);
+
+}
